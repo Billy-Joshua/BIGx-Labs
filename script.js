dd14f852debd1b9aacf9058cc519f2f43608223f
Billy19092024 <billyjoshuaishimwe@gmail.com>
2025-08-27 14:26:17 +0000

    #day5 done js arithmetic

---
+++ script.js
@@ -0,0 +1,4 @@
+let text1 ="A"
+let text2 ="B"
+let result = text1 < text2; // returns true because "A" is less than "B"
+document.getElementById("demo2").innerHTML ="is A less than B?" + result;       
