7f346ae2db60d595a12ddec5fbc1b22f1bdc404d
Billy19092024 <billyjoshuaishimwe@gmail.com>
2025-10-29 16:43:47 +0200

    added #day17 in js RegExp

--- script.js
+++ script.js
@@ -260,21 +260,7 @@ const person5 ={
     }
 }
 document.getElementById("demo40").innerHTML = person5.fullname();
-// javascript math
-document.getElementById("demo41").innerHTML = 
-"<p><b>Math.E:</b> " + Math.E + "</p>" +
-"<p><b>Math.PI:</b> " + Math.PI + "</p>" +
-"<p><b>Math.SQRT2:</b> " + Math.SQRT2 + "</p>" +
-"<p><b>Math.SQRT1_2:</b> " + Math.SQRT1_2 + "</p>" +
-"<p><b>Math.LN2:</b> " + Math.LN2 + "</p>" +
-"<p><b>Math.LN10:</b> " + Math.LN10 + "</p>" +
-"<p><b>Math.LOG2E:</b> " + Math.LOG2E + "</p>" +
-"<p><b>Math.Log10E:</b> " + Math.LOG10E + "</p>";
-
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-// #day16 RegExp javascript 
+// #day16 RegExp javascript
 
 let thisworld ="hello, my name is Joshua";
 let pattern =/Billy/; 
